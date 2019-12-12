dec5334890ed58f21982db3ebae48380247cdbf4
adamwinzdesign <adamwinzdesign@gmail.com>
2019-12-12 16:22:10 -0600

    finished main reducer

--- reduxcustomapp/src/index.js
+++ reduxcustomapp/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 import reducer from './reducers';
 
